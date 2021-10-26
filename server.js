82bfaff5c615335580fca767ff4205065ddda00b
Ali A <alsamawiali4@gmail.com>
2021-10-25 22:30:43 -0400

    home-ejs

--- server.js
+++ server.js
@@ -12,9 +12,16 @@ const app = express()
 // Mount Middleware (app.use)
 
 // Mount routes
+app.get('/home', function (req, res){
+  res.send("<h1> Ali's Home lol,</h1>")
+})
+
+app.get('/', function (req, res){
+  res.send("<h1> hello ALi,</h1>")
+})
 
 // Tell the app to listen on port 3000
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000')
+app.listen(3001, function() {
+  console.log('Listening on port 3001')
 })

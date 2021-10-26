5b6e824aa7657584c78c95130c58edf332e94298
Ali A <alsamawiali4@gmail.com>
2021-10-25 21:47:00 -0400

    test

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+
 // import modules
 
 import express from 'express'
 
-//
-console.log('')
+// Create Express app
+
+const app = express()
+
+// Configure the app (app.set)
+
+// Mount Middleware (app.use)
+
+// Mount routes
+
+// Tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
